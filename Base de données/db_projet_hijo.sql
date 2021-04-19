defa5927c861978ac371b7f0f269070a060d9ddd
Joachim <etu44137@henallux.be>
2021-04-19 21:11:02 +0200

    commit maj DB

--- Base de données/db_projet_hijo.sql
+++ Base de données/db_projet_hijo.sql
@@ -20,7 +20,49 @@ CREATE TABLE customer(
     CONSTRAINT num_customer_pk PRIMARY KEY(num_customer),
     CONSTRAINT email_customer UNIQUE(email)
 );
-CREATE TABLE bill();
-CREATE TABLE article();
-CREATE TABLE aisle();
-CREATE TABLE listing();
+CREATE TABLE bill(
+	id_bill INTEGER,
+    date_bill DATE NOT NULL,
+    is_discount_before_deadline BOOLEAN NOT NULL,
+    discount_before_deadline DOUBLE,
+    discount_coupon INTEGER,
+    possible_remarks VARCHAR(20),
+    employee INTEGER,
+    customer INTEGER,
+    CONSTRAINT id_bill_pk PRIMARY KEY(id_bill),
+    CONSTRAINT employee_fk
+		FOREIGN KEY(employee_fk)
+        REFERENCES employee(num_employee),
+	CONSTRAINT customer_fk
+		FOREIGN KEY(customer_fk)
+        REFERENCES customer(num_customer)
+);
+CREATE TABLE article(
+	id_article INTEGER,
+    wording VARCHAR(20) NOT NULL,
+    price DOUBLE NOT NULL,
+    vat DOUBLE NOT NULL,
+    aisle INTEGER NOT NULL,
+    CONSTRAINT id_article_pk PRIMARY KEY(id_article),
+    CONSTRAINT aisle_fk
+		FOREIGN KEY(aisle_fk)
+        REFERENCES aisle(num_aisle)
+);
+CREATE TABLE aisle(
+	num_aisle INTEGER,
+    wording VARCHAR(20) NOT NULL,
+    nb_shelves INTEGER NOT NULL,
+    CONSTRAINT num_aisle_pk PRIMARY KEY(num_aisle)
+);
+CREATE TABLE listing(
+	quantity INTEGER NOT NULL,
+    price DOUBLE NOT NULL,
+    bill INTEGER,
+    article INTEGER,
+    CONSTRAINT bill_fk
+		FOREIGN KEY(bill_fk)
+        REFERENCES bill(id_bill),
+    CONSTRAINT article_fk
+		FOREIGN KEY(article_fk)
+		REFERENCES article(id_article)
+);
